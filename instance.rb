eafc2e54221109249c8a85ee61151be5260ead3d
kappa <kappa@kappair-3.local>
2013-06-25 22:56:53 +0900

    add terminal-table

--- instance.rb
+++ instance.rb
@@ -5,6 +5,7 @@
 require 'rubygems'
 require 'yaml'
 require 'aws-sdk'
+require 'terminal-table'
 
 #
 config = YAML.load(File.read("config.yml"))
@@ -14,7 +15,13 @@ AWS.config(config)
 ec2 = AWS::EC2.new(config)
 
 #
-puts "id\t\thypervisor\tipaddress" 
+rows = []
 ec2.instances.each do |instance|
-  puts "#{instance.id}\t#{instance.hypervisor}\t\t#{instance.ip_address}"
+  rows << ["#{instance.id}","#{instance.hypervisor}","#{instance.ip_address}"]
 end
+
+#
+table = Terminal::Table.new :headings => ['id', 'hypervisor','ipaddress'], :rows => rows
+
+#
+puts table
