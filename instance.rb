f7053de57fb89290778379aad80f39833fc883bf
kappa <kappa@x1carbon.inokara.com>
2013-06-26 07:34:31 +0900

    modified.

--- instance.rb
+++ instance.rb
@@ -6,7 +6,7 @@ require 'rubygems'
 require 'yaml'
 require 'aws-sdk'
 require 'terminal-table'
-
+require 'pp'
 #
 config = YAML.load(File.read("config.yml"))
 AWS.config(config)
@@ -17,11 +17,17 @@ ec2 = AWS::EC2.new(config)
 #
 rows = []
 ec2.instances.each do |instance|
-  rows << ["#{instance.id}","#{instance.hypervisor}","#{instance.ip_address}"]
+#ec2.instances.each_with_index do |instance,i|
+  rows << ["#{instance.id}","#{instance.hypervisor}","#{instance.ip_address}","#{instance.status}"]
+#  puts "#{i}"
+#puts instance.methods.sort
+puts (instance.methods - instance.class.superclass.instance_methods).sort
+#puts instance.class
+#puts instance.class.superclass
 end
 
 #
-table = Terminal::Table.new :headings => ['id', 'hypervisor','ipaddress'], :rows => rows
+table = Terminal::Table.new :headings => ['id', 'hypervisor','ipaddress','status'], :rows => rows
 
 #
 puts table
