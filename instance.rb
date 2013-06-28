96d5f1b89dae0b028098eb88fb6c36dc3d4932f8
YoheiKawahara <inokara@gmail.com>
2013-06-28 19:38:16 +0900

    選択できるようにします。

--- instance.rb
+++ instance.rb
@@ -15,15 +15,24 @@ AWS.config(config)
 ec2 = AWS::EC2.new(config)
 
 #
+COMMAND=["start", "stop", "list"]
+unless ARGV.size == 1
+  puts "Usage: #{$0} ${command(#{COMMAND.join(' or ')})}"
+  exit 1
+end
+
+##################### list
 rows = []
 #ec2.instances.each do |instance|
 ec2.instances.each_with_index do |instance,i|
   rows << ["#{i}","#{instance.id}","#{instance.hypervisor}","#{instance.ip_address}","#{instance.status}"]
   #puts (instance.methods - instance.class.superclass.instance_methods).sort # for debug
 end
-
 #
 table = Terminal::Table.new :headings => ['no','id', 'hypervisor','ipaddress','status'], :rows => rows
-
 #
 puts table
+
+##################### start
+##################### stop
+
